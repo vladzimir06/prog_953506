25bbea0dd72ec936e8699a0c0b72dc9138747c22
StreletsWorkA <59307803+StreletsWorkA@users.noreply.github.com>
2020-05-24 20:38:28 +0300

    added labs 5,6 (#53)
    * added labs 5,6
    
    * Update Laboratory6(C).cpp
    
    * fixed

---
+++ Strelets/lab_5/5.1.cpp
@@ -0,0 +1,250 @@
+﻿//Вариант 21
+#include <iostream>
+
+
+struct list
+{
+    int field;
+    struct list* next;
+    struct list* prev;
+};
+
+struct list* Init(int a)
+{
+    struct list* lst;
+    lst = (struct list*)malloc(sizeof(struct list));
+    lst->field = a;
+    lst->next = NULL;
+    lst->prev = NULL;
+    return(lst);
+}
+
+struct list* Addelem(list* lst, int number)
+{
+    struct list* temp, * p;
+    temp = (struct list*)malloc(sizeof(list));
+    p = lst->next;
+    lst->next = temp;
+    temp->field = number;
+    temp->next = p;
+    temp->prev = lst;
+    if (p != nullptr)
+        p->prev = temp;
+    return(temp);
+}
+
+void Listprint(list* lst)
+{
+    struct list* p;
+    p = lst;
+    do {
+        printf("%d ", p->field);
+        p = p->next;
+    } while (p != NULL);
+}
+
+void Count(int& a, int b)
+{
+    while (b)
+    {
+        b /= 10;
+        a++;
+    }
+}
+
+int Equals(list* lst, list* lst2)
+{
+    if (lst->field == lst2->field)
+        return 1;
+    else if (lst->field < lst2->field)
+        return -1;
+    else
+        return 0;
+}
+
+int List_size(list* lst)
+{
+    int size = 0;
+    while (lst)
+    {
+        size++;
+        lst = lst->next;
+    }
+    return size;
+}
+
+void CompareList(list* lst, list* lst2)
+{
+    int size = List_size(lst);
+    int size2 = List_size(lst2);
+    int equal = 0;
+    bool cycler = true;
+    if (size == size2)
+    {
+        while (cycler)
+        {
+            equal = Equals(lst, lst2);
+            if (equal != 1)
+            {
+                switch (equal)
+                {
+                case 0: printf_s("First is bigger"); break;
+                case -1: printf_s("Second is bigger"); break;
+                }
+                cycler = false;
+                break;
+            }
+            else
+            {
+                lst = lst->next;
+                lst2 = lst2->next;
+            }
+        }
+
+    }
+    else
+    {
+        if (size > size2)
+            printf_s("First number is bigger");
+        else
+            printf_s("Second number is bigger");
+    }
+}
+
+struct list* ToLastPointer(list* lst)
+{
+    while (true)
+    {
+        lst = lst->next;
+        if (lst->next == nullptr)
+            break;
+    }
+    return lst;
+}
+
+void LongModShort(list* lst, int num)
+{
+    lst = ToLastPointer(lst);
+    int answer = 0, i = 1;
+
+    while (true)
+    {
+        if (lst->field == 0) break;
+        answer += lst->field % num;
+        lst = lst->prev;
+        lst->field *= powl(10, i);
+        i++;
+    }
+
+    if (answer >= num)
+        answer %= num;
+
+    printf_s("Your answer: %d", answer);
+}
+
+void LongDivShort(list* lst, short int num)
+{
+    lst = ToLastPointer(lst);
+    int answer = 0, i = 0;
+    bool cycler = true;
+    while (cycler)
+    {
+        if (lst->field == 0) break;
+        answer += lst->field;
+        i++;
+        lst = lst->prev;
+    }
+    answer /= num;
+    printf_s("Your answer = %d", answer);
+}
+
+void LongMulShort(list* lst, int num, list* lst2)
+{
+    lst = ToLastPointer(lst);
+    bool cycler = true;
+    int answer = 0;
+    while (cycler)
+    {
+        if (lst->field == 0) break;
+        answer += lst->field;
+        lst->field = 0;
+        lst = lst->prev;
+    }
+    answer *= num;
+    while (answer)
+    {
+        int curr = answer % 10;
+        answer /= 10;
+        Addelem(lst2, curr);
+    }
+}
+
+void Delete(list* lst)
+{
+    lst = ToLastPointer(lst);
+    while (lst)
+    {
+        lst = lst->prev;
+        free(lst->next);
+    }
+}
+
+
+
+int main()
+{
+    printf_s("Enter first number ");
+    int number = 0;
+    scanf_s("%d", &number);
+    printf_s("Enter second number ");
+    int number2 = 0;
+    scanf_s("%d", &number2);
+    int counter = 0, counter2 = 0, cnt = 0;
+    printf_s("Enter short number for all operations ");
+    int divider = 0;
+    scanf_s("%d", &divider);
+    list firstnum = *Init(0);
+    list secondnum = *Init(0);
+    list thirdnum = *Init(0);
+
+    Count(counter, number);
+    Count(counter2, number2);
+
+    while (number)
+    {
+        int curr = number % 10;
+        number /= 10;
+        Addelem(&firstnum, curr);
+    }
+    while (number2)
+    {
+        int curr = number2 % 10;
+        number2 /= 10;
+        Addelem(&secondnum, curr);
+    }
+
+    list firstnum2 = firstnum;
+    list secondnum2 = secondnum;
+
+    CompareList(&firstnum, &secondnum);
+
+    printf_s("\n");
+    Listprint(&firstnum2);
+    printf_s("\n");
+    Listprint(&secondnum2);
+    printf_s("\n");
+    LongModShort(&firstnum2, divider);
+    printf_s("\n");
+    LongDivShort(&firstnum2, divider);
+    printf_s("\n");
+    LongMulShort(&firstnum, divider, &thirdnum);
+    Listprint(&thirdnum);
+
+    Delete(&firstnum);
+    Delete(&secondnum);
+    Delete(&firstnum2);
+    Delete(&secondnum2);
+    Delete(&thirdnum);
+
+}
+
---
+++ Strelets/lab_6/Laboratory6(C).cpp
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int COUNT = 10;
+
+struct Tree
+{
+	int key;
+	unsigned char height;
+	Tree* left, *right;
+	Tree(int value) { key = value; left = right = 0; height = 1; }
+}*root;
+
+unsigned char Height(Tree* pointer)
+{
+	return pointer ? pointer->height : 0;
+}
+
+int Bfactor(Tree* pointer)
+{
+	return Height(pointer->right) - Height(pointer->left);
+}
+
+void Fixheight(Tree* pointer)
+{
+	unsigned char symbol1 = Height(pointer->left);
+	unsigned char symbol2 = Height(pointer->right);
+	pointer->height = (symbol1 > symbol2 ? symbol1 : symbol2) + 1;
+}
+
+Tree* Rotateright(Tree* pointer) 
+{
+	Tree* helper = pointer->left;
+	pointer->left = helper->right;
+	helper->right = pointer;
+	Fixheight(pointer);
+	Fixheight(helper);
+	return helper;
+}
+
+Tree* Rotateleft(Tree* check)
+{
+	Tree* pointer = check->right;
+	check->right = pointer->left;
+	pointer->left = check;
+	Fixheight(check);
+	Fixheight(pointer);
+	return pointer;
+}
+
+Tree* Balance(Tree* pointer)
+{
+	Fixheight(pointer);
+	if (Bfactor(pointer) == 2)
+	{
+		if (Bfactor(pointer->right) < 0)
+			pointer->right = Rotateright(pointer->right);
+		return Rotateleft(pointer);
+	}
+	if (Bfactor(pointer) == -2)
+	{
+		if (Bfactor(pointer->left) > 0)
+			pointer->left = Rotateleft(pointer->left);
+		return Rotateright(pointer);
+	}
+	return pointer;
+}
+
+Tree* Insert(Tree* pointer, int value)
+{
+	if (!pointer)
+	{
+		Tree* p = (Tree*)malloc(sizeof(Tree));
+		p->key = value;
+		return p;
+	}
+	if (value < pointer->key)
+		pointer->left = Insert(pointer->left, value);
+	else
+		pointer->right = Insert(pointer->right, value);
+	return Balance(pointer);
+}
+
+Tree* Findmin(Tree* pointer)
+{
+	return pointer->left ? Findmin(pointer->left) : pointer;
+}
+
+Tree* Removemin(Tree* pointer)
+{
+	if (pointer->left == 0)
+		return pointer->right;
+	pointer->left = Removemin(pointer->left);
+	return Balance(pointer);
+}
+
+Tree* Remove(Tree* pointer, int value) 
+{
+	if (!pointer) return 0;
+	if (value < pointer->key)
+		pointer->left = Remove(pointer->left, value);
+	else if (value > pointer->key)
+		pointer->right = Remove(pointer->right, value);
+	else 
+	{
+		Tree* check = pointer->left;
+		Tree* help = pointer->right;
+		delete pointer;
+		if (!help) return check;
+		Tree* min = Findmin(help);
+		min->right = Removemin(help);
+		min->left = check;
+		return Balance(min);
+	}
+	return Balance(pointer);
+}
+
+void Printl(Tree* root, int space)
+{
+
+	if (root == NULL)
+		return;
+	space += COUNT;
+	Printl(root->right, space);
+	cout << endl;
+	for (int i = COUNT; i < space; i++)
+		cout << " ";
+	cout << root->key << "\n";
+	Printl(root->left, space);
+}
+
+void DeleteAll(Tree* t) 
+{
+	if (t != NULL)
+	{
+		DeleteAll(t->left);
+		DeleteAll(t->right);
+		free(t);
+	}
+}
+
+int main()
+{
+	root = (Tree*)malloc(sizeof(Tree));
+	root->left = root->right = 0;
+	root->height = 1;
+	root->key = 0;
+	while (true)
+	{
+		int value = 0;
+		cin >> value;
+		if (value == 0)
+			break;
+		root = Insert(root, value);
+	}
+	Printl(root, 0);
+	DeleteAll(root);
+}
+
