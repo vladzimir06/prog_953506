3a764e733be2ec480ab390b4d38bcda9f4f2eecb
victoriastelmashuk <62291292+victoriastelmashuk@users.noreply.github.com>
2020-06-09 23:44:20 +0300

    LR1-7

---
+++ Стельмашук Виктория/LR5/LR5.cpp
@@ -0,0 +1,215 @@
+﻿// Задание 5.1(21) Стельмашук В.А.(гр. №953506)
+
+/*Создать тип данных Многоразрядное число.Разработать следующие
+функции :
+· Equal() – сравнение двух многоразрядных чисел(возвращает 0,
+если числа равны, 1 – если первое число больше, -1 – если второе
+число больше);
+· LongModShort() – возвращает остаток от деления
+многоразрядного числа на короткое число типа int;
+· LongDivShort() – возвращает результат целочисленного деления
+многоразрядного числа на короткое число типа int;
+· LongMulShort() – возвращает результат умножения
+многоразрядного числа на короткое число типа int.
+Для хранения многоразрядного числа использовать динамический
+двунаправленный список.*/
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <conio.h>
+#include <iostream>
+
+/* Структура, описывающая элемент двунаправленного списка */
+typedef struct item
+{
+    int digit;          
+    struct item* next,* prev;   //указатель на сдедующий и предыдущий элемент
+} Item;
+
+/* Структура, описывающая многоразрядное число */
+typedef struct mnumber
+{
+    Item* head, * tail;     //голова,хвост
+    int n;
+} MNumber;
+
+MNumber CreateMNumber(char* initStr);
+void AddDigit(MNumber* number, int digit);
+void PrintMNumber(MNumber number); 
+MNumber LongDivShort(MNumber n1, int del);
+MNumber LongModShort(MNumber n1, int del);
+int Equal(MNumber p1, MNumber p2);
+MNumber LongMulShort(MNumber n1, int x);
+void Remove(MNumber number);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char n1[1000];
+    char n2[1000];
+    int n3;
+    printf("Введите первое многорозрядное число!(Long(1))\n");
+    gets_s(n1);
+    MNumber a = CreateMNumber(n1);
+    printf("Введите второе многорозрядное число!(Long(2))\n");
+    gets_s(n2);
+    MNumber b = CreateMNumber(n2);
+    printf("Введите число типа int!(short)\n");
+    scanf_s("%d", &n3);
+    printf("Результат сравнения = %d.\n", Equal(a, b));
+    MNumber c = LongDivShort(a, n3);
+    printf("LongDivShort = ");
+    PrintMNumber(c);
+    Remove(c);
+    MNumber c1 = LongModShort(a, n3);
+    printf("LongModShort = ");
+    PrintMNumber(c1);
+    Remove(c);
+    printf("Long(1)MulShort = ");
+    MNumber d1 = LongMulShort(a, n3);
+    PrintMNumber(d1);
+    Remove(d1);
+    printf("Long(2)MulShort = ");
+    MNumber d2 = LongMulShort(b, n3);
+    PrintMNumber(d2);
+    Remove(d2);
+    Remove(a);
+    Remove(b);
+}
+
+MNumber CreateMNumber(char initStr[])//создает многоразрядное число из цифр строки
+{
+    MNumber number = { NULL, NULL, 0 };
+    int n;
+    for (n = strlen(initStr) - 1; n >= 0; n--)
+        AddDigit(&number, initStr[n] - '0');
+    return number;
+}
+
+void AddDigit(MNumber* number, int digit)//добавляет цифру в многоразрядное число
+{
+    Item* p = (Item*)malloc(sizeof(Item));
+    p->digit = digit;
+    p->next = p->prev = NULL;
+    if (number->head == NULL)
+        number->head = number->tail = p;
+    else
+    {
+        number->tail->next = p;
+        p->prev = number->tail;
+        number->tail = p;
+    }
+    number->n++;
+}
+
+int Equal(MNumber p1, MNumber p2)//сравнение двух многоразрядных чисел(0 - числа равны, 1 – первое число больше, -1 – второе число больше)
+{
+    int x1 = 0, x2 = 0;
+    Item* P1 = p1.head;
+    Item* P2 = p2.head;
+    while (P1)
+    {
+        x1++;
+        P1 = P1->next;
+    }
+    while (P2)
+    {
+        x2++;
+        P2 = P2->next;
+    }
+    if (x1 > x2) return 1;
+    if (x1 < x2) return -1;
+    if (x1 = x2)
+    {
+        P1 = p1.head;
+        P2 = p2.head;
+        while (P1 && P2)
+        {
+            x1 = P1->digit;
+            x2 = P2->digit;
+            if (x1 > x2) return 1;
+            if (x1 < x2) return -1;
+            P1 = P1->next;
+            P2 = P2->next;
+        }
+        return 0;
+    }
+
+}
+
+MNumber LongModShort(MNumber n1, int del)//возвращает остаток от деления многоразрядного числа на короткое число типа int
+{
+    char a[2] = "";
+    MNumber mod = CreateMNumber(a);
+    Item* p1 = n1.tail;
+    int digit, s1 = 0;
+    while (p1)
+    {
+        if (p1) { s1 += p1->digit; p1 = p1->prev; }
+        digit = s1 / del;
+        s1 = (s1 % del) * 10;
+    }
+    AddDigit(&mod, s1 / 10);
+    return mod;
+}
+MNumber LongDivShort(MNumber n1, int del)//возвращает результат целочисленного деления многоразрядного числа на короткое число типа int
+{
+    char a[2] = "";
+    MNumber div = CreateMNumber(a);
+    Item* p1 = n1.tail;
+    int digit, s1 = 0;
+    while (p1)
+    {
+        if (p1) { s1 += p1->digit; p1 = p1->prev; }
+        digit = s1 / del;
+        AddDigit(&div, digit);
+        s1 = (s1 % del) * 10;
+    }
+    return div;
+}
+
+MNumber LongMulShort(MNumber n1, int x)//возвращает результат умножения многоразрядного числа на короткое число типа int.
+{
+    char a[2] = "";
+    MNumber mul = CreateMNumber(a);
+    Item* p1 = n1.head;
+    int s1, digit = 0, pos = 0;
+    int mod = 0;
+    mod = x % 10;
+    x /= 10;
+    if (mod != 0)
+    {
+        while (p1)
+        {
+            if (p1) { s1 = p1->digit; p1 = p1->next; }
+            digit = (s1 * mod + pos) % 10;
+            pos = (s1 * mod + pos) / 10;
+            AddDigit(&mul, digit);
+        }
+        if (pos) AddDigit(&mul, pos);
+        LongMulShort(mul, mod);
+    }
+    return mul;
+}
+
+void PrintMNumber(MNumber number)// выводит многоразрядное число на экран
+{
+    Item* p = number.head;
+    while (p)
+    {
+        printf("%d", p->digit);
+        p = p->next;
+    }
+    printf("\n");
+}
+void Remove(MNumber number)//удаляет структуру
+{
+    Item* temp;
+    while (number.head)
+    {
+        temp = number.head;
+        number.head = number.head->next;
+        free(temp);
+    }
+    free(number.head);
+}
---
+++ Стельмашук Виктория/LR6/LR6.cpp
@@ -0,0 +1,126 @@
+﻿//Задание 5.2(14) Стельмашук В.А.(гр. №953506)
+
+/*В текстовом файле содержится произвольный текст. Построить на
+его основе бинарное дерево поиска, каждый узел которого
+содержит слово. Вывести из дерева на печать слова, начинающиеся
+и заканчивающиеся на одну и ту же букву, упорядочив их по
+алфавиту. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <locale.h>
+
+/* Структура, описывающая узел дерева */
+struct tree_node
+{
+    char word[100];
+    struct tree_node* left;
+    struct tree_node* right;
+};
+
+void treeremove(tree_node* root); 
+struct tree_node* Add(struct tree_node* root, char* word);
+int Check_Word(char* word);
+void Find(struct tree_node** Tree_1, struct tree_node** Tree_2);
+void Print_Tree(struct tree_node* tree);
+
+int main()
+{
+    struct tree_node* root = NULL;
+    char word[100];
+    FILE* file;
+    if (fopen_s(&file, "C:\\Лабораторные С\\LR6\\text.txt", "r"))
+    {
+        printf("\nError.");
+        char c = getchar();
+        exit(-1);
+    }
+    while (fgets(word, 20, file))
+    {
+        root = Add(root, word);
+    }
+    fclose(file);
+    struct tree_node* Sorted_words = NULL;
+    Find(&root, &Sorted_words);
+    printf("Words begin and end with the same letter:\n\n");
+    Print_Tree(Sorted_words);
+    treeremove(root);
+    return 0;
+}
+
+/* Добавить узел в бинарное дерево поиска */
+struct tree_node* Add(struct tree_node* root, char* word) 
+{
+    int temp;
+    if (!root) 
+    {
+        if (!(root = (struct tree_node*)malloc(sizeof(struct tree_node)))) 
+        {
+            printf("Error with memory allocation");
+        }
+        else {
+            root->left = root->right = NULL;
+            strcpy_s(root->word, word);
+        }
+    }
+    else if ((temp = strcmp(word, root->word)) < 0) 
+    {
+        root->left = Add(root->left, word);
+    }
+    else 
+    {
+        root->right = Add(root->right, word);
+    }
+    return root;
+}
+
+/*функция для проверки слов начинающихся и оканчивающихся одной буквой*/
+int Check_Word(char* word) 
+{
+    char first = word[0];
+    char last;
+    int i;
+    for (i = 0; word[i]; i++);
+    last = word[i - 2];
+    if (last == first) return 1;
+    else return 0;
+}
+
+/*функция для нахождения слов начинающихся и оканчивающихся одной буквой*/
+void Find(struct tree_node** Tree_1, struct tree_node** Tree_2)
+{
+    if ((*Tree_1) != NULL)
+    {
+        Find(&(*Tree_1)->left, &(*Tree_2));
+        if (Check_Word((*Tree_1)->word)) {
+            (*Tree_2) = Add((*Tree_2), (*Tree_1)->word);
+        }
+        Find(&(*Tree_1)->right, &(*Tree_2));
+    }
+}
+
+/* Обход дерева слева (вывод по алфавиту))*/
+void Print_Tree(struct tree_node* tree) 
+{
+    if (tree->left)
+    {
+        Print_Tree(tree->left);
+    }
+    printf("%s\n", tree->word);
+    if (tree->right)
+    {
+        Print_Tree(tree->right);
+    } 
+}
+
+void treeremove(tree_node* root)// удаляет дерево
+{
+    if (root != NULL)
+    {
+        treeremove(root->left);
+        treeremove(root->right);
+        free(root);
+    }
+}
