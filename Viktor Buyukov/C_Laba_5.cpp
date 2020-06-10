36310701db460797b5e4ac19ef7a753179af4f75
ViktorVangok <62500742+ViktorVangok@users.noreply.github.com>
2020-06-11 02:45:33 +0300

    Laba_3 and Laba_5 (#21)

---
+++ Viktor Buyukov/C_Laba_5.cpp
@@ -0,0 +1,215 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+
+#define FALSE 0;
+#define TRUE 1;
+
+typedef struct item
+{
+    int position;
+    int data;
+    struct item* next;
+}Item;
+
+int Push(int numeric);
+int Pop (int numeric);
+void Display();
+void Change(int numeric);
+
+Item* top = NULL;
+
+int main()
+{
+    int done = FALSE;
+    int numeric = 0;
+    char choice;
+
+    while (!done)
+    {
+        Display();
+        printf("\n\n A-Add \nD-Delete \nC-Change \nQ-Quit");
+        choice = _getch();
+
+        switch (toupper(choice))
+        {
+        case 'A':numeric = Push(numeric); break;
+        case 'D':numeric = Pop(numeric); break;
+        case 'C':Change(numeric); break;
+        case 'Q':done = TRUE; break;
+        }
+    }
+
+    return 0;
+}
+
+int Push(int numeric)
+{
+    Item* p;
+
+    p = (Item*)malloc(sizeof(Item));
+    p->data = rand();
+    p->position = ++numeric;
+    p->next = top;
+    top = p;   
+
+    return numeric;
+}
+
+int Pop(int numeric)
+{
+    Item* p;
+
+    if (top != NULL)
+    {
+        p = top;
+        top = top->next;
+        free(p);
+        --numeric;
+
+        return numeric;
+    }
+
+    return numeric;
+}
+
+void Display()
+{
+    Item* p = top;
+
+    system("cls");
+
+    if (p == NULL) printf("Stack is empty");
+    else
+    {
+        printf("Stack:");
+        while (p!=NULL)
+        {
+            printf("\n(%d) -> %d", p->position, p->data);
+            p = p->next;
+        }
+    }   
+}
+
+void Change(int numeric)
+{
+    int element_for_change1 = 0, element_for_change2 = 0, condition = (numeric + 1)/2, const elements_of_stack = numeric ;
+    bool exit = false;
+
+    for (int n=0;n<condition;n++)
+    {
+        exit = false;
+        Item* p = top;
+        if (p == NULL) { printf("Stack is empty"); }
+        else
+        {
+            while (exit == false)
+            {
+                if (numeric + 1 == p->position) p->data = element_for_change2;
+                if (numeric == p->position) element_for_change1 = p->data;
+                if (numeric + p->position == elements_of_stack + 1)
+                {
+                    element_for_change2 = p->data;
+                    p->data = element_for_change1;
+
+                    exit = true;
+                    --numeric;   
+
+                    if ((n == condition - 1) && (elements_of_stack % 2 == 0))
+                    {
+                        Item* p = top;
+                        while (numeric + 1 != p->position) p = p->next;
+                        
+                        p->data = element_for_change2;
+                    }
+                }
+
+                p = p->next;                   
+            }
+        }
+    }
+    numeric = elements_of_stack;
+}
+
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+
+#define FALSE 0;
+#define TRUE 1;
+
+typedef struct item
+{
+    int data;
+    struct item* next;
+}Item;
+
+void Push();
+void Pop();
+void Display();
+
+Item* top = NULL;
+
+int main()
+{
+    int done = FALSE;
+    char choice;
+
+    while (!done)
+    {
+        Display();
+        printf("\n\n A-Add \nD-Delete \nQ-Quit");
+        choice = _getch();
+
+        switch (toupper(choice))
+        {
+        case 'A':Push(); break;
+        case 'D':Pop(); break;
+        case 'Q':done = TRUE; break;
+        }
+    }
+
+    return 0;
+}
+
+void Push()
+{
+    Item* p;
+
+    p = (Item*)malloc(sizeof(Item));
+    p->data = rand();
+    p->next = top;
+    top = p;
+}
+
+void Pop()
+{
+    Item* p;
+
+    if (top != NULL)
+    {
+        p = top;
+        top = top->next;
+        free(p);
+    }
+}
+
+void Display()
+{
+    Item* p = top;
+
+    system("cls");
+
+    if (p == NULL) printf("Stack is empty");
+    else
+    {
+        printf("Stack:");
+        while (p!=NULL)
+        {
+            printf("\n%d", p->data);
+            p = p->next;
+        }
+    }
+}*/
---
+++ Viktor Buyukov/Output_C_Laba3.cpp
@@ -0,0 +1,83 @@
+﻿//Laba#3  Variant 25 (Буюков Виктор 953506)
+//Куб состоит из n^3 прозрачных и непрозрачных элементарных 
+//кубиков. Построить полностью непрозрачный куб, используя ровно 
+//n^2 непрозрачных элементарных кубиков.
+
+#include <stdio.h>
+#include <malloc.h>
+#include <math.h>
+
+
+int simple_check(int size)
+{
+    int exit = 0; 
+    int size_new_massiv = 0;
+    int amount_of_circle = sqrt(size); // граница возможного варианта движения
+    double b; // рабочая переменная
+
+        for (int i = 2; i <= amount_of_circle; i++)
+        {
+            exit = 0;
+            b = (double)size;
+            while (exit < 3)
+            {
+                b = b / i; 
+                exit++;
+            }
+            if (b == 1)
+            {
+                size_new_massiv = i * i;
+                printf("\n\nWe find chislo.\nSize_New_Massiv = %d", size_new_massiv);
+                i = ++amount_of_circle;
+            }
+            else printf("\n");
+        }
+    
+    if (size_new_massiv == 0)
+    {
+        printf("Right input -> 8 27 64 125 (n^3)\n");
+    }
+
+    return size_new_massiv;
+}
+
+void massiv(int size_new_massiv, int amount_elements_of_massiv) // построение необходимого кубика
+{
+    int*** massiv;
+    massiv = (int***)malloc(size_new_massiv *  sizeof(int));
+    for (int i = 0; i < size_new_massiv; i++)
+    {
+        massiv[i] = (int**)malloc(size_new_massiv * sizeof(int));
+        for (int j = 0; j < size_new_massiv; j++)
+        {
+            massiv[i][j] = (int*)malloc(size_new_massiv * sizeof(int));
+            for (int k = 0; k < size_new_massiv; k++)
+            {
+                massiv[i][j][k] = 1;
+                printf("\nmassiv[%d][%d][%d] = %d",i,j,k,massiv[i][j][k]);
+            }
+        }
+    }    
+}
+
+
+int main()
+{
+    int size; // высота куба, состоящего из n^3 кубиков
+    int size_new_massiv = 0; // высота нового куба, состоящего из n^2 кубиков
+  
+    size_new_massiv = simple_check(size); // проверка возможно ли из size создать new size удовлетворающая условие
+    if (size_new_massiv == 0) return 0; // программа с подобным результатом не может работать 
+
+    int amount_elements_of_massiv;
+    printf("\n\nA cube consists of n transparent and opaque elementary cubes\nWrite number of orague cubes =");
+    scanf_s("%d", &amount_elements_of_massiv);
+
+    while (amount_elements_of_massiv < size * size)
+    {
+        printf("\nMinimum %d.Try again =", size * size);
+        scanf_s("%d", &amount_elements_of_massiv);
+    }
+
+    massiv(size_new_massiv, amount_elements_of_massiv);
+}
